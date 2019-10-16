e94a46e45bd9166c2eba3f77e3da597c0cd89d08
Ross Nelson <RossN430@gmail.com>
2019-10-16 18:31:25 -0500

    Added timeline landing page

--- csc394/Timeline/views.py
+++ csc394/Timeline/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+# Timeline view
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def timeline_index(request):
+    return HttpResponse("<h1> This is the Timeline app")
