8fd6478509b0fff220c590b36f2043cc5481c2b9
kylem164 <kylemastrangeli@gmail.com>
2019-11-24 18:50:32 -0600

    updated html: database created and connected

--- csc394/csc394/urls.py
+++ csc394/csc394/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from tasks.views import taskView, addTodo, editTodo, deleteTodo, todoDetails
+from reviews.views import addReview
 
 
 urlpatterns = [
@@ -27,5 +28,6 @@ urlpatterns = [
     path('editTodo/<int:todo_id>/', editTodo),
     path('deleteTodo/<int:todo_id>/', deleteTodo),
     path('tasks/<int:todo_id>/', todoDetails),
-    path('reviews/',include('reviews.urls'))
+    path('reviews/',include('reviews.urls')),
+    path('addReview/',addReview)
 ]
--- csc394/reviews/views.py
+++ csc394/reviews/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Review
 
 # Create your views here.
 
 def index(request):
-    return render(request,'reviews/forms.html')
+    return render(request,'reviews/newReview.html')
+
+def addReview(request):
+    newReview = Review(name=request.POST['task'],performance=request.POST['performance'],
+    improvement=request.POST['improvement'],additionalInfo=request.POST['additionalInfo'])
+    newReview.save()
+    return HttpResponseRedirect('/reviews/')
     
